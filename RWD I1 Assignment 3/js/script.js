8098aba6074d790affb7112f1881a73a394ce37f
vidwalk <claudiurediu@gmail.com>
2017-11-29 11:41:59 +0100

    RWD assignment 3

--- RWD I1 Assignment 3/js/script.js
+++ RWD I1 Assignment 3/js/script.js
@@ -1,135 +1,163 @@
-$("#fish1Id").ready(function () {
-    animateDiv("#fish1Id");
+// moving around of the orange fish
 
-});
-$("#fish2Id").ready(function () {
-    animateDiv("#fish2Id");
-
-});
-$("#fish1Id").dblclick(function(){
-    grow("#fish1Id");
-    
-});
-$("#fish2Id").mouseover(function () {
-
-    randomMov("#fish2Id");
-})
-$("#bubble1Id").ready(function () {
-    animateBubbl("#bubble1Id");
-})
-$("#bubble2Id").ready(function () {
-    animateBubbl("#bubble2Id");
-})
-$("#bubble3Id").ready(function () {
-    animateBubbl("#bubble3Id");
-})
-$("#bubble1Id").click(function () {
-    $("#bubble1Id").stop().fadeOut(2000);
-    $("#bubble1Id").show(10, function () {
-        animateBubbl("#bubble1Id");
-    });
-
-});
-$("#bubble2Id").click(function () {
-    $("#bubble2Id").stop().fadeOut(2000);
-    $("#bubble2Id").show(10, function () {
-        animateBubbl("#bubble2Id");
-    });
-
-});
-$("#bubble3Id").click(function () {
-    $("#bubble3Id").stop().fadeOut(2000);
-    $("#bubble3Id").show(10, function () {
-        animateBubbl("#bubble3Id");
-    });
-
-});
-$(document).click(function (e) {
-    var mouseX = e.pageX - $("#fish1Id").width()/2;
-    var mouseY = e.pageY - $("#fish1Id").height()/2;
-    follow("#fish1Id", mouseX, mouseY);
+$('#fish1Id').ready(function () {
+    movingAround('#fish1Id');
 });
 
-// function
+// moving around of the blue fish
 
-function makeNewPosition(IdRef) {
+$('#fish2Id').ready(function () {
+    movingAround('#fish2Id');
+});
 
+// blue fish animation on hover
 
-    var h = $(window).height() - $(IdRef).height();
-    var w = $(window).width() - $(IdRef).width();
-    var nh = Math.floor(Math.random() * h);
-    var nw = Math.floor(Math.random() * w);
+$('#fish2Id').hover(function () {
+    var position = randomPosition(this);
+    $(this).stop().animate({
+        top: position[0],
+        left: position[1]
+    }, 800, function () {
+        movingAround(this)
+    })
+});
 
-    return [nh, nw];
+// orange fish double click resize
 
-}
+$('#fish1Id').dblclick(function () {
+    doubleClickFish(this);
+});
 
-function animateDiv(IdRef2) {
-    var newq = makeNewPosition(IdRef2);
-    $(IdRef2).animate({
-        top: newq[0],
-        left: newq[1]
-    }, 6000, function () {
-        animateDiv(IdRef2);
-    });
+// moving orange fish on the screen by clicking
 
-};
-
-function animateBubbl(IdRef4) {
-    var width = $(window).width() - $(IdRef4).width();
-    var bx = Math.floor((Math.random() * width) + 1)
-    var delay = 9000 + Math.floor(Math.random() * 10000);
-    $(IdRef4).css("top", "100%");
-    $(IdRef4).css("left", bx);
-    $(IdRef4).animate({
-        top: "-11%"
-    }, delay, function () {
-        animateBubbl(IdRef4);
-    });
-}
-
-function randomMov(IdRef3) {
-    var pos = makeNewPosition(IdRef3);
-    $(IdRef3).stop().animate({
-            top: pos[0] / 4,
-            left: pos[1] / 4
-        }, "slow",
-        function () {
-            animateDiv(IdRef3);
-        });
-}
-
-function follow(IdRef6, x, y) {
-    
-    $(IdRef6).stop().animate({
-        left: x - 10,
-        top: y - 10
+$(document).click(function (event) {
+    var x = event.pageX - 125;
+    var y = event.pageY - 125;
+    $('#fish1Id').stop().animate({
+        top: y,
+        left: x
     }, function () {
-        animateDiv(IdRef6);
+        movingAround(this)
+    })
+
+
+});
+
+// Bubbles click fades them out, fades in, then moves up again
+
+$('#bubble1Id').click(function () {
+    $(this).stop().fadeOut(200, function () {
+        $(this).fadeIn();
+        moveBubble('#bubble1Id')
+    });
+});
+$('#bubble2Id').click(function () {
+    $(this).stop().fadeOut(200, function () {
+        $(this).fadeIn();
+        moveBubble('#bubble2Id')
+    });
+});
+$('#bubble3Id').click(function () {
+    $(this).stop().fadeOut(200, function () {
+        $(this).fadeIn();
+        moveBubble('#bubble3Id')
+    });
+});
+$('#bubble4Id').click(function () {
+    $(this).stop().fadeOut(200, function () {
+        $(this).fadeIn();
+        moveBubble('#bubble4Id')
+    });
+});
+
+// Bubbles moving initialize
+
+$('#bubble1Id').ready(function () {
+    moveBubble('#bubble1Id');
+});
+$('#bubble2Id').ready(function () {
+    setTimeout(function () {
+        moveBubble('#bubble2Id')
+    }, 2500);
+});
+$('#bubble3Id').ready(function () {
+    setTimeout(function () {
+        moveBubble('#bubble3Id')
+    }, 3500);
+});
+$('#bubble4Id').ready(function () {
+    setTimeout(function () {
+        moveBubble('#bubble4Id')
+    }, 1000);
+});
+
+// Double click to resize the orange fish
+
+function doubleClickFish(fish) {
+
+    $(fish).stop().animate({
+        width: '+=100px',
+        height: '+=100px'
+    }).delay(800).animate({
+        width: '-=100px',
+        height: '-=100px'
+    }, function () {
+        movingAround(fish)
     });
 };
 
-function grow(IdRef7) {
-    var size = $(IdRef7).height();
-    size = size*1.5;
-    $(IdRef7).stop().animate({
-        height: size,
-        width: 'auto'
-    }, 3000,function(){
-        setTimeout(function() {   
-            shrink("#fish1Id");
-         }, 3000);
-        animateDiv(IdRef7);
+// Hover the blue fish for it to move
+
+function movingAround(fish) {
+    var position = randomPosition(fish);
+    $(fish).stop().animate({
+        top: position[0],
+        left: position[1]
+    }, 6000, function () {
+        movingAround(fish)
+    })
+}
+
+function randomPosition(fish) {
+    var x = Math.floor(Math.random() * ($(document).width() - $(fish).width()));
+    var y = Math.floor(Math.random() * ($(document).height() - $(fish).height()));
+    console.log(x + " " + y)
+    var array = [y, x];
+    return array;
+}
+
+
+$(document).ready(function () {
+    var bubbles = [$('#bubble1Id'), $('#bubble2Id'), $('#bubble3Id'), $('#bubble4Id')]
+    for (var i = 0; i <= 3; i++) {
+        bubbles[i].offset({
+            top: $(document).height()
+        })
+
+    }
+});
+// set prime position of the bubble
+
+
+
+function moveBubble(bubble) {
+    var bubbleStartPosition;
+    bubbleStartPosition = randomBubblePosition(bubble);
+    $(bubble).stop().offset({
+        top: bubbleStartPosition[1],
+        left: bubbleStartPosition[0]
     });
-    
-};
-function shrink(IdRef8){
-    var smallSize = $(IdRef8).height();
-    smallSize = smallSize/1.5;
-    $(IdRef8).stop().animate({
-        height: smallSize,
-        width: 'auto'
-    }, 3000,function(){
-        animateDiv(IdRef8);
+    $(bubble).stop().animate({
+        top: -$(document).height() + $(bubble).height()
+    }, 12000, 'linear', function () {
+        moveBubble(bubble)
     });
-};
+}
+
+function randomBubblePosition(bubble) {
+    var x = Math.floor(Math.random() * ($(document).width() - $(bubble).width()));
+    var y = $(document).height();
+    console.log("bubble" + x + " " + y)
+    var array = [x, y];
+    return array;
+}
