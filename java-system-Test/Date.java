e59c1f1a0e6bffffe59588d8ca7c97f4cae3c165
vidwalk <claudiurediu@gmail.com>
2017-12-14 15:13:42 +0100

    JavaDoc done, use case fixed, project report structure

--- java-system-Test/Date.java
+++ java-system-Test/Date.java
@@ -1,98 +1,155 @@
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.io.Serializable;
-public class Date implements Serializable
-{
-   private int day;
-   private int month;
-   private int year;
 
-   public Date(int day, int month, int year)
-   {
-      this.year = year;
-      this.month = month;
-      this.day = day;
-   }
+public class Date implements Serializable {
+	private int day;
+	private int month;
+	private int year;
 
-   public Date()
-   {
-      Calendar calendar = GregorianCalendar.getInstance();
-      this.year = calendar.get(Calendar.YEAR);
-      this.month = calendar.get(Calendar.MONTH) + 1;
-      this.day = calendar.get(Calendar.DAY_OF_MONTH);
-   }
+	/**
+	 * Set value to day,month and year
+	 * 
+	 * @author IT-1V-A17-Group2
+	 */
+	public Date(int day, int month, int year) {
+		this.year = year;
+		this.month = month;
+		this.day = day;
+	}
 
-   public int getDay()
-   {
-      return day;
-   }
+	/**
+	 * Set current date
+	 * 
+	 * @author IT-1V-A17-Group2
+	 */
+	public Date() {
+		Calendar calendar = GregorianCalendar.getInstance();
+		this.year = calendar.get(Calendar.YEAR);
+		this.month = calendar.get(Calendar.MONTH) + 1;
+		this.day = calendar.get(Calendar.DAY_OF_MONTH);
+	}
 
-   public void setDay(int day)
-   {
-      if((day>0)&&(day<32)) this.day=day;
-   }
+	/**
+	 * Return day
+	 * 
+	 * @author IT-1V-A17-Group2
+	 */
+	public int getDay() {
+		return day;
+	}
 
-   public int getMonth()
-   {
-      return month;
-   }
+	/**
+	 * Create or change day
+	 * 
+	 * @author IT-1V-A17-Group2
+	 */
+	public void setDay(int day) {
+		if ((day > 0) && (day < 32))
+			this.day = day;
+	}
 
-   public void setMonth(int month)
-   {  if((month>0)&&(month<13)) this.month=month;
-      switch (month)
-      {
-         case 1: this.day=31;
-         case 2:{if ((((this.year % 4 == 0) && (this.year % 100 != 100))|| (this.year % 400 == 0))) this.day=28;
-         else this.day=29;}
-         case 3:this.day=31;
-         case 4:this.day=30;
-         case 5:this.day=31;
-         case 6:this.day=30;
-         case 7:this.day=31;
-         case 8:this.day=31;
-         case 9:this.day=30;
-         case 10:this.day=31;
-         case 11:this.day=30;
-         case 12:this.day=31;
+	/**
+	 * Return month
+	 * 
+	 * @author IT-1V-A17-Group2
+	 */
+	public int getMonth() {
+		return month;
+	}
 
-      }
-   }
+	/**
+	 * Create or change month,and check how many days are in this month
+	 * 
+	 * @author IT-1V-A17-Group2
+	 */
+	public void setMonth(int month) {
+		if ((month > 0) && (month < 13))
+			this.month = month;
+		switch (month) {
+		case 1:
+			this.day = 31;
+		case 2: {
+			if ((((this.year % 4 == 0) && (this.year % 100 != 100)) || (this.year % 400 == 0)))
+				this.day = 28;
+			else
+				this.day = 29;
+		}
+		case 3:
+			this.day = 31;
+		case 4:
+			this.day = 30;
+		case 5:
+			this.day = 31;
+		case 6:
+			this.day = 30;
+		case 7:
+			this.day = 31;
+		case 8:
+			this.day = 31;
+		case 9:
+			this.day = 30;
+		case 10:
+			this.day = 31;
+		case 11:
+			this.day = 30;
+		case 12:
+			this.day = 31;
 
-   public int getYear()
-   {
-      return year;
-   }
+		}
+	}
 
-   public void setYear(int year)
-   {
-      if (year > this.year)
-         this.year = year;
-   }
+	/**
+	 * Return year
+	 * 
+	 * @author IT-1V-A17-Group2
+	 */
+	public int getYear() {
+		return year;
+	}
 
-   public String toString()
-   {
-      if (month < 10)
-         return (day + "/0" + month + "/" + year);
-      else
-         return (day + "/" + month + "/" + year);
-   }
+	/**
+	 * Create or change year
+	 * 
+	 * @author IT-1V-A17-Group2
+	 */
+	public void setYear(int year) {
+		if (year > this.year)
+			this.year = year;
+	}
 
-   @Override
-   public boolean equals(Object obj)
-   {
-      if (this == obj)
-         return true;
-      if (obj == null)
-         return false;
-      if (getClass() != obj.getClass())
-         return false;
-      Date other = (Date) obj;
-      if (day != other.day)
-         return false;
-      if (month != other.month)
-         return false;
-      if (year != other.year)
-         return false;
-      return true;
-   }
+	/**
+	 * Return all variables in a short sentence(all variables are string)
+	 * 
+	 * @author IT-1V-A17-Group2
+	 */
+	public String toString() {
+		if (month < 10)
+			return (day + "/0" + month + "/" + year);
+		else
+			return (day + "/" + month + "/" + year);
+	}
+
+	/**
+	 * Whether two dates are the same or not
+	 * 
+	 * @author IT-1V-A17-Group2
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date other = (Date) obj;
+		if (day != other.day)
+			return false;
+		if (month != other.month)
+			return false;
+		if (year != other.year)
+			return false;
+		return true;
+	}
 }
--- java-system-Test/FileHandler.java
+++ java-system-Test/FileHandler.java
@@ -8,224 +8,220 @@ import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.io.Serializable;
+
 /**
  * A static class for file handling
+ * 
  * @author IT-1V-A17-Group2
  */
-public final class FileHandler  implements Serializable
-{
+public final class FileHandler implements Serializable {
 
-   public static File eventFile;
-   public FileHandler()
-   {
-      eventFile = new File("events.bin");
-   }
-   public static void saveMembersMailsToDoc(String membersEmails)
-   {
-      String filename = "membersEmails.txt";
-      File file = new File(filename);
-      try {
-         PrintWriter out = new PrintWriter(file); // Open the file
+	public static File eventFile;
 
+	/**
+	 * Create a file
+	 * 
+	 * @author IT-1V-A17-Group2
+	 */
+	public FileHandler() {
+		eventFile = new File("events.bin");
+	}
 
-               out.print(membersEmails);
-               out.flush(); // Force it to write the text
-               out.close(); // Close the file
-               System.out.println("End writing data to file: " + file.getAbsolutePath());
-      }
-      catch(FileNotFoundException e)
-      {
+	/**
+	 * Create a file for writing members' email address
+	 * 
+	 * @author IT-1V-A17-Group2
+	 */
+	public static void saveMembersMailsToDoc(String membersEmails) {
+		String filename = "membersEmails.txt";
+		File file = new File(filename);
+		try {
+			PrintWriter out = new PrintWriter(file); // Open the file
 
-      }
+			out.print(membersEmails);
+			out.flush(); // Force it to write the text
+			out.close(); // Close the file
+			System.out.println("End writing data to file: " + file.getAbsolutePath());
+		} catch (FileNotFoundException e) {
 
+		}
 
-   }
+	}
 
-   public static void saveMembers(Member[] members)
-   {
-      ObjectOutputStream out = null;
-      try
-      {
-      File file = new File("members.bin");
-      FileOutputStream fos = new FileOutputStream(file, false);
-      out = new ObjectOutputStream(fos);
-      System.out.println("Saving to a file");
-      for(int i=0;i<members.length;i++)
-      {
-         out.writeObject(members[i]);
-      }
+	/**
+	 * Create a file to save a list of members
+	 * 
+	 * @author IT-1V-A17-Group2
+	 */
+	public static void saveMembers(Member[] members) {
+		ObjectOutputStream out = null;
+		try {
+			File file = new File("members.bin");
+			FileOutputStream fos = new FileOutputStream(file, false);
+			out = new ObjectOutputStream(fos);
+			System.out.println("Saving to a file");
+			for (int i = 0; i < members.length; i++) {
+				out.writeObject(members[i]);
+			}
 
-      }
-      catch(IOException e)
-      {
-         e.printStackTrace();
-      }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-      finally
-      {
-      try
-      {
-      out.close();
-      }
-      catch (IOException e)
-      {
-      e.printStackTrace();
-      }
-      }
-   }
+		finally {
+			try {
+				out.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-   public static void saveMember(Member member)
-   {
-      ObjectOutputStream out = null;
-      try
-      {
-      File file = new File("members.bin");
-      FileOutputStream fos = new FileOutputStream(file);
-      out = new ObjectOutputStream(fos);
+	/**
+	 * Create a file to save members
+	 * 
+	 * @author IT-1V-A17-Group2
+	 */
+	public static void saveMember(Member member) {
+		ObjectOutputStream out = null;
+		try {
+			File file = new File("members.bin");
+			FileOutputStream fos = new FileOutputStream(file);
+			out = new ObjectOutputStream(fos);
 
-         out.writeObject(member);
+			out.writeObject(member);
 
-      }
-      catch(IOException e)
-      {
-         e.printStackTrace();
-      }
-      finally
-      {
-      try
-      {
-      out.close();
-      }
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				out.close();
+			}
 
-      catch (IOException e)
-      {
-      e.printStackTrace();
-      }
-      }
-   }
-// get members array
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-  public static Member[] getMembers()
-  {
-     ArrayList<Object> membersList = new ArrayList<Object>();
-     boolean cont = true;
-     try{
+	/**
+	 * Output all members in the file
+	 * 
+	 * @author IT-1V-A17-Group2
+	 */
 
-        FileInputStream fis = new FileInputStream("members.bin");
-        ObjectInputStream input = new ObjectInputStream(fis);
+	public static Member[] getMembers() {
+		ArrayList<Object> membersList = new ArrayList<Object>();
+		boolean cont = true;
+		try {
 
-        while(cont){
-           Object obj = input.readObject();
-           if(obj != null)
-              membersList.add(obj);
-           else
-              cont = false;
-        }
-     }catch(Exception e){
-        //System.out.println(e.printStackTrace());
-     }
+			FileInputStream fis = new FileInputStream("members.bin");
+			ObjectInputStream input = new ObjectInputStream(fis);
 
-     return membersList.toArray(new Member[membersList.size()]);
+			while (cont) {
+				Object obj = input.readObject();
+				if (obj != null)
+					membersList.add(obj);
+				else
+					cont = false;
+			}
+		} catch (Exception e) {
+			// System.out.println(e.printStackTrace());
+		}
 
-  }
+		return membersList.toArray(new Member[membersList.size()]);
 
-   public static void saveEvents(Event[] events)
-   {
-      ObjectOutputStream out = null;
-      try
-      {
+	}
 
-         FileOutputStream fos = new FileOutputStream("events.bin");
-         out = new ObjectOutputStream(fos);
+	/**
+	 * Save a list of events into the file
+	 * 
+	 * @author IT-1V-A17-Group2
+	 */
+	public static void saveEvents(Event[] events) {
+		ObjectOutputStream out = null;
+		try {
 
-         for(int i=0;i<events.length;i++)
-         {
-            out.writeObject(events[i]);
-         }
+			FileOutputStream fos = new FileOutputStream("events.bin");
+			out = new ObjectOutputStream(fos);
 
-      }
-      catch(IOException e)
-      {
-         e.printStackTrace();
-      }
+			for (int i = 0; i < events.length; i++) {
+				out.writeObject(events[i]);
+			}
 
-      finally
-      {
-         try
-         {
-            out.close();
-         }
-         catch (IOException e)
-         {
-            e.printStackTrace();
-         }
-      }
-   }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-   public static void saveEvent(Event event)
-   {
-      ObjectOutputStream out = null;
-      try
-      {
+		finally {
+			try {
+				out.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-         FileOutputStream fos = new FileOutputStream("events.bin");
-         out = new ObjectOutputStream(fos);
+	/**
+	 * Save an event to the file
+	 * 
+	 * @author IT-1V-A17-Group2
+	 */
+	public static void saveEvent(Event event) {
+		ObjectOutputStream out = null;
+		try {
 
-         out.writeObject(event);
+			FileOutputStream fos = new FileOutputStream("events.bin");
+			out = new ObjectOutputStream(fos);
 
-      }
-      catch(IOException e)
-      {
-         e.printStackTrace();
-      }
-      finally
-      {
-         try
-         {
-            out.close();
-         }
+			out.writeObject(event);
 
-         catch (IOException e)
-         {
-            e.printStackTrace();
-         }
-      }
-   }
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				out.close();
+			}
 
-// get events array
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-   public static Event[] getEvents()
-   {
-      ArrayList<Object> eventsList = new ArrayList<Object>();
-      boolean cont = true;
-      try{
+	/**
+	 * Return a list of events from the file
+	 * 
+	 * @author IT-1V-A17-Group2
+	 */
+	public static Event[] getEvents() {
+		ArrayList<Object> eventsList = new ArrayList<Object>();
+		boolean cont = true;
+		try {
 
-         FileInputStream fis = new FileInputStream("events.bin");
-         ObjectInputStream input = new ObjectInputStream(fis);
+			FileInputStream fis = new FileInputStream("events.bin");
+			ObjectInputStream input = new ObjectInputStream(fis);
 
-         while(cont){
-            Object obj = input.readObject();
-            if(obj instanceof Event)
-               eventsList.add(obj);
-            else
-               cont = false;
-         }
-            try
-            {
-               input.close();
-            }
+			while (cont) {
+				Object obj = input.readObject();
+				if (obj instanceof Event)
+					eventsList.add(obj);
+				else
+					cont = false;
+			}
+			try {
+				input.close();
+			}
 
-            catch (IOException e)
-            {
-               e.printStackTrace();
-            }
+			catch (IOException e) {
+				e.printStackTrace();
+			}
 
-      }catch(Exception e){
-         //System.out.println(e.printStackTrace());
-      }
+		} catch (Exception e) {
+			// System.out.println(e.printStackTrace());
+		}
 
-      return eventsList.toArray(new Event[eventsList.size()]);
+		return eventsList.toArray(new Event[eventsList.size()]);
 
-   }
+	}
 }
-
